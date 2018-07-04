45b0a206dd8e78efa475792683ae0d03c84f9a28
Lisakova Anastasiia <a.lisakova@osla.ru>
2018-07-04 19:38:58 +0300

    first components styled

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".prices-slider").slick({
+    infinite: true,
+    arrows: true,
+    // autoplay: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+});
