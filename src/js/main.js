716f75c45e4092dbd265ffdcb4548bec3ec00240
alisakova <coctail_rossini@mail.ru>
2018-07-07 16:15:56 +0300

    adaptive begin

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
     // autoplay: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
   });
 });
